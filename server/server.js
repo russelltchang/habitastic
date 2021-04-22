d0bb6989e6cd1ecee8cebf67c3169edd9919afa0
Russell <Russell@Russells-MacBook-Pro.local>
2021-04-22 15:21:39 -0700

    static adjust

--- server/server.js
+++ server/server.js
@@ -30,7 +30,11 @@ app.use(passport.session());
 //goes to dist for css/js files that index.html requires
 app.use(express.static(path.resolve(__dirname, "../dist")));
 
-app.use("/logo.png", express.static(path.join(__dirname, "client/public")));
+app.use(
+  "/logo.png",
+  express.static(path.resolve(__dirname, "../client/public"))
+);
+console.log(path.join(path.resolve(__dirname, "../client/public")));
 
 app.use(cors({ credentials: true, origin: "http://localhost:8080" }));
 app.use(express.json());
