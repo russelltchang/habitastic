c84997491cb4de99f8b0a348431bb14854147238
Russell <Russell@Russells-MacBook-Pro.local>
2021-10-20 16:27:02 -0700

    defineplugin change

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,13 +4,13 @@ const webpack = require("webpack");
 const dotenv = require("dotenv");
 
 // call dotenv and it will return an Object with a parsed key
-const env = dotenv.config().parsed;
+// const env = dotenv.config().parsed;
 
 // reduce it to a nice object, the same as before
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
-}, {});
+// const envKeys = Object.keys(env).reduce((prev, next) => {
+//   prev[`process.env.${next}`] = JSON.stringify(env[next]);
+//   return prev;
+// }, {});
 
 module.exports = {
   mode: "production",
@@ -61,7 +61,11 @@ module.exports = {
       filename: "[name].css",
       chunkFilename: "[id].css",
     }),
-    new webpack.DefinePlugin(envKeys),
+    new DefinePlugin({
+      "process.env.PROD_CLIENT_ID": JSON.stringify(
+        "AfUSLOnx-8lzDv0tL4fDB5Z5W0-_euwEtDGHR4pUZrlYMfJ8Zxr0AzOGKt-OxqA4OGVfkpUURlncKLe_"
+      ),
+    }),
   ],
   externals: {
     cheerio: "window",
